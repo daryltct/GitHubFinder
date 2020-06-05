6601128e2879fcd0402dc9ecdd551d010b80a567
Daryl Tan Chin Ting <daryltanct@gmail.com>
2020-06-06 04:06:50 +0800

    Add NavBar component

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import NavBar from './components/layouts/NavBar';
 
 function App() {
 	return (
-		<div className="App">
+		<div>
+			<NavBar />
 			<h1>hello world</h1>
 		</div>
 	);
---
+++ app/src/components/layouts/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function NavBar() {
+	return (
+		<nav className="navbar bg-primary">
+			<h1>
+				<i className="fab fa-github" /> GitHubFinder
+			</h1>
+		</nav>
+	);
+}
+
+export default NavBar;
