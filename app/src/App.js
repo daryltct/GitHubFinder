d613794f0203e1faff60bfc5085b2203dcb50fb0
Daryl Tan Chin Ting <daryltanct@gmail.com>
2020-06-06 04:27:47 +0800

    Basic UserItem component

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import NavBar from './components/layouts/NavBar';
+import UserItem from './components/users/UserItem';
 
 function App() {
 	return (
 		<div>
 			<NavBar />
+			<UserItem />
 			<h1>hello world</h1>
 		</div>
 	);
