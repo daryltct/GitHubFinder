cb92cc244b1f9639441956bb8ac45c923e21cdeb
Daryl Tan Chin Ting <daryltanct@gmail.com>
2020-06-07 05:26:12 +0800

    User details styling

--- app/src/components/users/User.js
+++ app/src/components/users/User.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useContext } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useContext, Fragment } from 'react';
+import { useParams, Link } from 'react-router-dom';
 
 import { UserContext } from '../../context/UserContext';
 import Spinner from '../layouts/Spinner';
@@ -14,6 +14,7 @@ function User() {
 
 	const {
 		name,
+		company,
 		avatar_url,
 		location,
 		bio,
@@ -26,7 +27,65 @@ function User() {
 		hireable
 	} = userDetails;
 
-	return loading ? <Spinner /> : <div>{name}</div>;
+	return loading ? (
+		<Spinner />
+	) : (
+		<Fragment>
+			<Link to="/" className="btn btn-light">
+				Back to Search Results
+			</Link>
+			Hireable: <i className={hireable ? 'fas fa-check text-success' : 'fas fa-times-circle text-danger'} />
+			<div className="card grid-2">
+				<div className="all-center">
+					<img src={avatar_url} className="round-img" alt="" style={{ width: 150 }} />
+					<h1>{name}</h1>
+					<p>Location: {location}</p>
+				</div>
+				<div>
+					{bio && (
+						<Fragment>
+							<h3>Bio</h3>
+							<p>{bio}</p>
+						</Fragment>
+					)}
+					<a href={html_url} className="btn btn-dark my-1">
+						Visit GitHub Profile
+					</a>
+					<ul>
+						<li>
+							{login && (
+								<Fragment>
+									<strong>Username: </strong> {login}
+								</Fragment>
+							)}
+						</li>
+
+						<li>
+							{company && (
+								<Fragment>
+									<strong>Company: </strong> {company}
+								</Fragment>
+							)}
+						</li>
+
+						<li>
+							{blog && (
+								<Fragment>
+									<strong>Website: </strong> {blog}
+								</Fragment>
+							)}
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div className="card text-center">
+				<div className="badge badge-primary">Followers: {followers}</div>
+				<div className="badge badge-success">Following: {following}</div>
+				<div className="badge badge-light">Public Repos:: {public_repos}</div>
+				<div className="badge badge-dark">Public Gists: {public_gists}</div>
+			</div>
+		</Fragment>
+	);
 }
 
 export default User;
