87ccdceaae903e2d59da61c4230061d653208e12
Daryl Tan Chin Ting <daryltanct@gmail.com>
2020-06-07 01:57:45 +0800

    Search functionality

--- app/src/UserContext.js
+++ app/src/UserContext.js
@@ -3,23 +3,32 @@ import axios from 'axios';
 
 const UserContext = React.createContext();
 
+const github = axios.create({
+	baseURL: 'https://api.github.com',
+	timeout: 2000,
+	headers: { Authorization: process.env.REACT_APP_GITHUB_TOKEN }
+});
+
 function UserContextProvider(props) {
 	const [ usersData, setUsersData ] = useState([]);
 	const [ loading, setLoading ] = useState(false);
 
-	useEffect(async () => {
+	async function searchUser(text) {
 		setLoading(true);
-		const github = axios.create({
-			baseURL: 'https://api.github.com',
-			timeout: 2000,
-			headers: { Authorization: process.env.REACT_APP_GITHUB_TOKEN }
-		});
-		const res = await github.get('/users');
-		setUsersData(res.data);
+		const res = await github.get(`/search/users?q=${text}`);
+		setUsersData(res.data.items);
 		setLoading(false);
-	}, []);
+	}
 
-	return <UserContext.Provider value={{ usersData, loading }}>{props.children}</UserContext.Provider>;
+	function clearUsers() {
+		setUsersData([]);
+	}
+
+	return (
+		<UserContext.Provider value={{ usersData, loading, searchUser, clearUsers }}>
+			{props.children}
+		</UserContext.Provider>
+	);
 }
 
 export { UserContextProvider, UserContext };
--- app/src/components/users/Search.js
+++ app/src/components/users/Search.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { UserContext } from '../../UserContext';
 
 function Search() {
+	const { usersData, searchUser, clearUsers } = useContext(UserContext);
 	const [ text, setText ] = useState('');
 
 	function handleChange(event) {
@@ -8,15 +10,26 @@ function Search() {
 		setText(value);
 	}
 
-	function handleSubmit(event) {
+	async function handleSubmit(event) {
 		event.preventDefault();
+		await searchUser(text);
+		setText('');
 	}
 
+	const showClear = usersData.length > 0 ? true : false;
+
 	return (
-		<form className="form" onSubmit={handleSubmit}>
-			<input type="text" placeholder="Search Users" value={text} onChange={handleChange} />
-			<input type="submit" value="Search" className="btn btn-dark btn-block" />
-		</form>
+		<div>
+			<form className="form" onSubmit={handleSubmit}>
+				<input type="text" placeholder="Search Users" value={text} onChange={handleChange} />
+				<input type="submit" value="Search" className="btn btn-dark btn-block" />
+			</form>
+			{showClear && (
+				<button className="btn btn-light btn-block" onClick={clearUsers}>
+					Clear
+				</button>
+			)}
+		</div>
 	);
 }
 
